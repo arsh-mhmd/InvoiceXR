d5153173d80e557ce91e071c46006ec79b61f481
Arshath Mohammed <amshahulhameed1@sheffield.ac.uk>
2021-03-17 12:49:47 +0000

    git commit -m "Initial Java Files Commit"

---
+++ src/main/java/invoice/xr/InvoiceXrApplication.java
@@ -0,0 +1,13 @@
+package invoice.xr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceXrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceXrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/invoice/xr/InvoiceXrApplicationTests.java
@@ -0,0 +1,13 @@
+package invoice.xr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceXrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
