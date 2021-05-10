cc3c24685bdb666c0200d6044a8c95e13056da59
Arshath Mohammed <amshahulhameed1@sheffield.ac.uk>
2021-05-10 17:50:18 +0100

    git commit -m "Payment update invoice Commit"

--- src/main/java/invoice/xr/controller/PaymentController.java
+++ src/main/java/invoice/xr/controller/PaymentController.java
@@ -1,21 +1,10 @@
 package invoice.xr.controller;
 
 import java.io.IOException;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
-import org.h2.util.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +24,8 @@ import invoice.xr.Utils.ApiResponse;
 import invoice.xr.Utils.URLUtils;
 import invoice.xr.config.PaypalPaymentIntent;
 import invoice.xr.config.PaypalPaymentMethod;
+import invoice.xr.model.InvoiceModel;
+import invoice.xr.service.InvoiceService;
 import invoice.xr.service.PaypalService;
 
 @Controller
@@ -49,9 +40,12 @@ public class PaymentController {
 	@Autowired
 	private PaypalService paypalService;
 	
+	@Autowired
+	private InvoiceService invoiceService;
+	
 	@GetMapping("pay")
 	public ResponseEntity<ApiResponse> pay(HttpServletRequest request, HttpServletResponse response, 
-			@RequestParam(name = "total") Double total) throws IOException{
+			@RequestParam(name = "total") Double total, @RequestParam(name = "invoiceNo") String invoiceNo) throws IOException{
 		String cancelUrl = URLUtils.getBaseURl(request) + "/" + PAYPAL_CANCEL_URL;
 		String successUrl = URLUtils.getBaseURl(request) + "/" + PAYPAL_SUCCESS_URL;
 		try {
@@ -60,7 +54,7 @@ public class PaymentController {
 					"GBP", 
 					PaypalPaymentMethod.paypal, 
 					PaypalPaymentIntent.sale,
-					"payment description", 
+					invoiceNo, 
 					cancelUrl, 
 					successUrl);
 			for(Links links : payment.getLinks()){
@@ -84,6 +78,9 @@ public class PaymentController {
 	public ModelAndView successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) throws IOException{
 		try {
 			Payment payment = paypalService.executePayment(paymentId, payerId);
+			Double paid = Double.parseDouble(payment.getTransactions().get(0).getAmount().getTotal());
+			String invoiceNo = payment.getTransactions().get(0).getDescription();
+			invoiceService.updateInvoicePayment(paid, invoiceNo);
 			if(payment.getState().equals("approved")){
 				return new ModelAndView("redirect:"+"http://localhost:8080/success");
 			}
