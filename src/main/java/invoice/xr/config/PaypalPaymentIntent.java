51c24328ab78d09fcbf6949ef24ab2795e1fc346
Arshath Mohammed <amshahulhameed1@sheffield.ac.uk>
2021-05-19 11:33:58 +0100

    Master Clear

--- src/main/java/invoice/xr/config/PaypalPaymentIntent.java
+++
@@ -1,7 +0,0 @@
-package invoice.xr.config;
-
-public enum PaypalPaymentIntent {
-
-	sale, authorize, order
-	
-}
--- src/main/java/invoice/xr/config/PaypalPaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package invoice.xr.config;
-
-public enum PaypalPaymentMethod {
-
-	credit_card, paypal
-	
-}
